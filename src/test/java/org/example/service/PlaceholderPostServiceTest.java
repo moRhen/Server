cb651c69f2d24864920b13bf47a39542d0f88b22
Paweł <morhenmail@gmail.com>
2023-01-15 08:58:33 +0100

    Add test for PlaceholderPostService

---
+++ src/test/java/org/example/service/PlaceholderPostServiceTest.java
@@ -0,0 +1,37 @@
+package org.example.service;
+
+import org.example.PostDto;
+import org.example.client.JPlaceholderClient;
+import org.example.repository.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest
+class PlaceholderPostServiceTest {
+
+  @InjectMocks PlaceholderPostService placeholderPostService;
+  @Mock
+  JPlaceholderClient jPlaceholderClient;
+  @Mock
+  private PostRepository postRepository;
+
+
+
+
+  @Test
+  void whenPostRequestedById_thenPostReturn() {
+    long postId = 1L;
+    when(jPlaceholderClient.getPostById(postId)).thenReturn(new PostDto(postId, 1, null, null));
+
+    PostDto post = placeholderPostService.getAndSavePost(postId);
+    assertNotNull(post);
+    assertEquals(postId, post.getId());
+  }
+}
